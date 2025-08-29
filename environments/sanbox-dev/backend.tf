2375655543f07ef2047c218024fd3b643f0ecbf9
AbdulRehman-Devops150 <abdulrehman.rbbn177@gmail.com>
2025-08-30 01:22:43 +0500

    Delete environments/sanbox-dev directory

--- environments/sanbox-dev/backend.tf
+++
@@ -1,9 +0,0 @@
-
-terraform {
-  backend "s3" {
-    bucket = "sandbox-dev-infra-tfstate-files"
-    key    = "sandbox-dev/terraform.tfstate"
-    region = "us-east-1"
-    # dynamodb_table = "terraform-state-lock"  # Optional: for state locking
-  }
-}
