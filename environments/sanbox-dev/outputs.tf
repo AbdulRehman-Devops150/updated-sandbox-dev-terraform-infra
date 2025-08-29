8e4f72fea8c124bb285b820b666ec4a9bfffc9d7
AbdulRehman-Devops150 <abdulrehman.rbbn177@gmail.com>
2025-08-30 00:59:10 +0500

    Create outputs.tf

---
+++ environments/sanbox-dev/outputs.tf
@@ -0,0 +1,59 @@
+
+# VPC Outputs
+output "vpc_id" {
+  description = "ID of the VPC"
+  value       = module.vpc.vpc_id
+}
+
+output "public_subnet_ids" {
+  description = "IDs of the public subnets"
+  value       = module.vpc.public_subnet_ids
+}
+
+output "private_subnet_ids" {
+  description = "IDs of the private subnets"
+  value       = module.vpc.private_subnet_ids
+}
+
+# Security Group Outputs
+output "alb_security_group_id" {
+  description = "ID of the ALB security group"
+  value       = module.security_groups.alb_security_group_id
+}
+
+output "ecs_security_group_id" {
+  description = "ID of the ECS security group"
+  value       = module.security_groups.ecs_security_group_id
+}
+
+# ALB Outputs
+output "alb_dns_name" {
+  description = "DNS name of the Application Load Balancer"
+  value       = module.alb.alb_dns_name
+}
+
+output "target_group_arn" {
+  description = "ARN of the target group"
+  value       = module.alb.target_group_arn
+}
+
+# ECS Outputs
+output "ecs_cluster_name" {
+  description = "Name of the ECS cluster"
+  value       = module.ecs.cluster_name
+}
+
+output "ecs_cluster_arn" {
+  description = "ARN of the ECS cluster"
+  value       = module.ecs.cluster_arn
+}
+
+output "task_execution_role_arn" {
+  description = "ARN of the ECS task execution role"
+  value       = module.ecs.task_execution_role_arn
+}
+
+output "task_role_arn" {
+  description = "ARN of the ECS task role"
+  value       = module.ecs.task_role_arn
+}
