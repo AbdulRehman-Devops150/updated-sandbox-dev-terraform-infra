4fd1e03c4809507d2cb7b69749e1c3a1c94a5ea8
AbdulRehman-Devops150 <abdulrehman.rbbn177@gmail.com>
2025-08-30 01:12:29 +0500

    Create variables.tf

---
+++ modules/security_groups/variables.tf
@@ -0,0 +1,10 @@
+
+variable "environment" {
+  description = "Environment name"
+  type        = string
+}
+
+variable "vpc_id" {
+  description = "VPC ID where security groups will be created"
+  type        = string
+}
